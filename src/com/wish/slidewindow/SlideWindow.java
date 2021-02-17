55d90cd1404b16536dbbffc4d5258798f8ff6c17
wish <weixi@bbdservice.com>
2021-02-17 22:21:50 +0800

    update

--- src/com/wish/slidewindow/SlideWindow.java
+++ src/com/wish/slidewindow/SlideWindow.java
@@ -15,35 +15,35 @@ public class SlideWindow {
         ArrayList<Integer> windowMax = new ArrayList<>();
         // 条件检查
         if (num == null || size < 1 || num.length < 1|| size > num.length) {
-        return windowMax;
-    }
+            return windowMax;
+        }
         Deque<Integer> indexQueue = new LinkedList<>();
         /**
          *         窗口还没有被填满时，找最大值的索引
          */
         for (int i = 0; i < size && i < num.length; i++) {
-        // 如果索引对应的值比之前存储的索引值对应的值大或者相等，就删除之前存储的值
-        while (!indexQueue.isEmpty() && num[i] >= num[indexQueue.getLast()]) {
-            indexQueue.removeLast();
+            // 如果索引对应的值比之前存储的索引值对应的值大或者相等，就删除之前存储的值
+            while (!indexQueue.isEmpty() && num[i] >= num[indexQueue.getLast()]) {
+                indexQueue.removeLast();
+            }
+            //  添加索引
+            indexQueue.addLast(i);
         }
-        //  添加索引
-        indexQueue.addLast(i);
-    }
         // 窗口已经被填满了
         for (int i = size; i < num.length; i++) {
-        // 第一个窗口的最大值保存
-        windowMax.add(num[indexQueue.getFirst()]);
-        // 如果新加入窗口的的值比队列中的值大，就删除队列的值
-        while (!indexQueue.isEmpty() && num[i] >= num[indexQueue.getLast()]) {
-            indexQueue.removeLast();
+            // 第一个窗口的最大值保存
+            windowMax.add(num[indexQueue.getFirst()]);
+            // 如果新加入窗口的的值比队列中的值大，就删除队列的值
+            while (!indexQueue.isEmpty() && num[i] >= num[indexQueue.getLast()]) {
+                indexQueue.removeLast();
+            }
+            // 删除队列中已经滑出窗口的数据对应的下标
+            if (!indexQueue.isEmpty() && indexQueue.getFirst() <= (i - size)) {
+                indexQueue.removeFirst();
+            }
+            // 可能的最大的下标索引入队
+            indexQueue.addLast(i);
         }
-        // 删除队列中已经滑出窗口的数据对应的下标
-        if (!indexQueue.isEmpty() && indexQueue.getFirst() <= (i - size)) {
-            indexQueue.removeFirst();
-        }
-        // 可能的最大的下标索引入队
-        indexQueue.addLast(i);
-    }
         // 最后一个窗口最大值入队
         windowMax.add(num[indexQueue.getFirst()]);
         return windowMax;
